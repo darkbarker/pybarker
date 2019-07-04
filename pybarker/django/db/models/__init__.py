4d51b8249129c78cfb277727c473cc7cc533ef94
darkbarker <barkdarker@gmail.com>
2019-07-04 19:50:04 +0500

    fix

--- pybarker/django/db/models/__init__.py
+++ pybarker/django/db/models/__init__.py
@@ -1,4 +1,4 @@
-from django.db.models.fields import *  # NOQA
-from django.db.models.fields import __all__ as fields_all
+from pybarker.django.db.models.fields import *  # NOQA
+from pybarker.django.db.models.fields import __all__ as fields_all
 
 __all__ = fields_all
