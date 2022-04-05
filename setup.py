4e55abf2787196ea332168918ba6dff21918a3d8
darkbarker <barkdarker@gmail.com>
2022-04-06 00:07:13 +0500

    setup fix exclude tests

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from os.path import join, dirname
 import pybarker
 
 setup(
-    name='pybarker',
+    name="pybarker",
     version=pybarker.__version__,
-    packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    packages=find_packages(exclude=["tests"]),
+    long_description=open(join(dirname(__file__), "README.md")).read(),
 )
