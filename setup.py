7c2a923bda47010f3395617ec8f2c766d93c4c75
darkbarker <barkdarker@gmail.com>
2022-04-28 18:27:38 +0500

    missing html fix

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     name="pybarker",
     version=pybarker.__version__,
     packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
     long_description=open(join(dirname(__file__), "README.md")).read(),
 )
