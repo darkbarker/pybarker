65a55d6cef8d6bd943dd4ab441293623a30266f7
darkbarker <barkdarker@gmail.com>
2022-12-13 01:37:48 +0500

    stats midl

--- pybarker/django/middleware/stats.py
+++ pybarker/django/middleware/stats.py
@@ -1,3 +1,4 @@
+import re
 import time
 import logging
 
@@ -5,6 +6,15 @@ from collections import defaultdict
 
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
+
+
+# with leading \n or "" if empty
+def _duplicates_format(dublicates, ch):
+    duplicates_log = [""]
+    for dublicate, num in dublicates.items():
+        if num > 1:
+            duplicates_log.append("%3d: %s" % (num, dublicate))
+    return f"\n {ch}".join(duplicates_log)
 
 
 class StatsMiddleware(MiddlewareMixin):
@@ -26,25 +36,24 @@ class StatsMiddleware(MiddlewareMixin):
         quertime = sum([float(q["time"]) for q in connection.queries])
         querlist = [q["sql"] for q in connection.queries]
 
-        dublicates = defaultdict(int)  # запрос: сколько таких
+        dublicates = defaultdict(int)  # {запрос: сколько таких}
+        dublicates_similar = defaultdict(int)  # то же самое, но для "похожих запросов"
         for query in querlist:
             dublicates[query] += 1
-
-        duplicates_log = [""] if getattr(settings, "STATS_THRESHOLD_LOG_DUPL", False) else None
+            # заменяем в query все числа на <N>
+            query_similar = re.sub("\d+", "<N>", query)
+            if query_similar != query:
+                dublicates_similar[query_similar] += 1
 
         response["X-Page-Generation-Duration-ms"] = curtime
         response["X-Page-Generation-Queries-Count"] = quercount
         response["X-Page-Generation-Queries-time"] = quertime
-        for num, dublicate in enumerate(dublicates):
-            if dublicates[dublicate] > 1:
-                # response["X-Page-Generation-Queries-dublicate-%s" % (num + 1)] = "%s ***** %s times" % (dublicate, dublicates[dublicate])
-                response["X-Page-Generation-Queries-dublicate-%s" % (num + 1)] = "%s" % (dublicates[dublicate])
-                if duplicates_log:
-                    duplicates_log.append("%3d: %s" % (dublicates[dublicate], dublicate))
-        # for num, query in enumerate(querlist):
-        #    response["X-Page-Generation-Queries-%s" % (num + 1)] = query
+
         # logging
         if curtime > settings.STATS_THRESHOLD_TIME or quercount > settings.STATS_THRESHOLD_QUERIES:
-            duplicates_log = "\n x".join(duplicates_log) if duplicates_log else ""
+            if getattr(settings, "STATS_THRESHOLD_LOG_DUPL", False):
+                duplicates_log = "%s%s" % (_duplicates_format(dublicates, "x"), _duplicates_format(dublicates_similar, "~"))
+            else:
+                duplicates_log = ""
             logging.getLogger("%s.StatsMiddleware" % __name__).warning("%s %s is too dumb: %s ms, %s queries%s" % (request.method, request.path, curtime, quercount, duplicates_log))
         return response
