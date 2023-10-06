eb258e704555ed07b717afb20cb1bc8f3a8d13dc
darkbarker <barkdarker@gmail.com>
2023-10-06 23:55:09 +0500

    p

--- pybarker/utils/exceptions.py
+++ pybarker/utils/exceptions.py
@@ -2,7 +2,8 @@ import sys
 import traceback
 
 
-# экзепшен в строку, как в интерпретаторе
+# экзепшен в строку, в точности как выглядит в интерпретаторе, аналог
+# format_exc, но форматирует переданное исключение, а не sys.exc_info
 if sys.version_info[0:2] >= (3, 10):
     def exception_to_string(ex, limit=None, chain=True):
         return "".join(traceback.format_exception(ex, limit=limit, chain=chain))
