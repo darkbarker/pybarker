4f38eb939bf6dc1348a449f45f76e661c4e08a3b
darkbarker <barkdarker@gmail.com>
2025-03-26 16:00:52 +0500

    FakeQueryCookieMiddleware

--- pybarker/contrib/channels/sessions.py
+++ pybarker/contrib/channels/sessions.py
@@ -5,6 +5,7 @@ class FakeQueryCookieMiddleware:
     """
     переписанный чанеловский CookieMiddleware который не читает куки, а читает параметр токен из кверистринга
     и делает единственный фейковый куку чтобы дальше в цепочке AuthMiddlewareStack всё работало
+    также кладёт разобранный токен в scope["__token"]
     """
 
     def __init__(self, inner):
@@ -18,4 +19,5 @@ class FakeQueryCookieMiddleware:
             if qs.startswith("token="):
                 qs = qs[6:]  # foo
                 cookies[settings.SESSION_COOKIE_NAME] = qs  # {'sessionid': 'foo'}
+                scope["__token"] = qs
         return await self.inner(dict(scope, cookies=cookies), receive, send)
